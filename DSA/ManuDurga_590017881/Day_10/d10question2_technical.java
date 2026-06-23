abc785958671f50bb64b187aa686672a979ade68
ManuDurga <you@example.com>
2026-06-24 00:19:45 +0530

    minor changes

--- DSA/ManuDurga_590017881/Day_10/d10question2_technical.java
+++ DSA/ManuDurga_590017881/Day_10/d10question2_technical.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class d10question2_technical {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();          // size of the input array
+        String s = sc.nextLine();  //String input
         boolean[] seen = new boolean[26];
         int count = 0;
 
